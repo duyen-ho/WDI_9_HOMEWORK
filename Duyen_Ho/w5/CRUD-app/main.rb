e6f2a556beea84eb476398b268939e914af62535
Duyen Ho <duyenho@gmail.com>
2016-10-30 23:56:05 +1100

    refactor code to integrate active record

--- Duyen_Ho/w5/CRUD-app/main.rb
+++ Duyen_Ho/w5/CRUD-app/main.rb
@@ -1,24 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pry'
-require 'pg'
+require_relative 'db_config'
+require_relative 'models/artwork'
+require_relative 'models/artwork_type'
+require_relative 'models/user'
+require_relative 'models/comment'
 
-
-def run_sql(sql)
-  db = PG.connect(dbname: 'lineart')
-  results = db.exec(sql)
-  db.close
-  return results
-end
-
-# Create
-# Read
-# Update
-# Delete
 
 # home - show all artworks
 get '/' do
-  @artworks = run_sql("SELECT * FROM artworks;")
+  @artworks = Artwork.all
+  # binding.pry
   erb :index
 end
 
@@ -29,36 +22,50 @@ end
 
 # C: to post new artwork
 post '/artworks' do
-  @artworks = run_sql("INSERT INTO artworks (title, artist, image_url) VALUES ('#{params[:title]}', '#{params[:artist]}', '#{params[:image_url]}');")
-  # redirect after posting
-  redirect to '/'
-  erb :artworks
+  artwork = Artwork.new
+  artwork.title = params[:title]
+  artwork.artist = params[:artist]
+  artwork.image_url = params[:image_url]
+
+  if artwork.save
+    redirect to '/'
+  else
+    erb :artworks
+  end
+
 end
 
 # R: to show artwork on own page
 get '/artworks/:id' do
-  results = run_sql("SELECT * FROM artworks WHERE id = #{ params[:id] };")
-  # search for artwork with that id (only one entry in array)
-  @artwork = results[0]
+  @artwork = Artwork.find(params[:id])
   erb :artworks_show
 end
 
 # U: to show the edit form
 get '/artworks/:id/edit' do
-  @artwork = run_sql("SELECT * FROM artworks WHERE id = #{ params[:id] };")[0]
+  @artwork = Artwork.find(params[:id])
   erb :artworks_edit
 end
 
 # U: to update edits
 post '/artworks/:id' do
-  run_sql("UPDATE artworks SET title = '#{ params[:title] }', artist = '#{ params[:artist] }', image_url = '#{ params[:image_url] }' WHERE id = #{ params[:id] };")
-  # dangerous operation so do:
-  redirect to "/artworks/#{ params[:id] }"
+  artwork = Artwork.find(params[:id])
+  artwork.title = params[:title]
+  artwork.artist = params[:artist]
+  artwork.image_url = params[:image_url]
 
+  if artwork.save
+    redirect to "/artworks/#{ params[:id] }"
+  else
+    erb :artworks_show
+  end
 end
 
 # D: to delete the artwork
 delete '/artworks/:id/delete' do
-  run_sql("DELETE FROM artworks WHERE id = #{ params[:id] };")
+  artwork = Artwork.find(params[:id])
+  artwork.delete
+  # destroy method has before and after callbacks
+
   redirect to '/'
 end
